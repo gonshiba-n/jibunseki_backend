62bbaeaf8f1286ff27839b1ad293c5f1bfe1d5b1
gonshiban <nss.0120g@gmail.com>
2021-03-21 09:56:40 +0900

    DB migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+  has_secure_password validations: true
+
+  # バリデーション
+  validates :name, presence: true, length: { maximum: 30 }
+  # メールアドレスの形か判定するための変数
+  email_check = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: email_check }
+  validates :password, length: { minimum: 6 }, on: :create
+
+  # トークン作成(英数+URLで使用可能な文字列の乱数生成)
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 乱数を暗号化(ハッシュ値生成)
+  def self.encrypt(token)
+    Digest::SHA256.hexdigest(token.to_s)
+  end
 end
--- db/migrate/20210320114137_create_users.rb
+++ db/migrate/20210320114137_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-
+      t.string :name, limit: 30, index: true, null: false
+      t.string :email, unique: true, null:false
+      t.string :password_digest, null:false
+      t.string :remember_token
       t.timestamps
     end
   end
