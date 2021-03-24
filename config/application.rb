c550b6983fd1e84ce853bb48d4ef11c3a0a655e1
gonshiban <nss.0120g@gmail.com>
2021-03-24 22:23:25 +0900

    Railsの日本語化対応

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,10 @@ module JibunsekiBackend
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Tokyo"
+    config.i18n.default_locale = :ja
+    config.active_record.default_timezone = :local
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
